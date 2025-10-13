4d1366ab0468ffe21df8aa7c5ab7e2ce65292e88
Regis <regis.lopes.eso@gmail.com>
2025-10-13 09:52:24 -0300

    Add Class 37: Using Axios to Get Products Data

--- Vue_Course_Material/Project 5 - Shopping Bag Initial/src/App.vue
+++ Vue_Course_Material/Project 5 - Shopping Bag Initial/src/App.vue
@@ -7,8 +7,14 @@
 </template>
 
 <script>
+  import axios from 'axios'
 
   export default {
+    created() {
+      axios.get('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
 
   }
   
