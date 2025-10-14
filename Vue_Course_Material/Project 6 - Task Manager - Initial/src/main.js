be97f2d7f10f27d04a6a503abb19051f8784e322
Regis <regis.lopes.eso@gmail.com>
2025-10-14 14:32:14 -0300

    Add Class 48: Options API x Composition API

---
+++ Vue_Course_Material/Project 6 - Task Manager - Initial/src/main.js
@@ -0,0 +1,10 @@
+import "./assets/reset.css";
+import "./assets/main.css";
+
+import { createApp } from "vue";
+
+import Test from "./Test.vue";
+
+const app = createApp(Test);
+
+app.mount("#app");
