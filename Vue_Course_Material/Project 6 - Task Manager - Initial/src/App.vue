57319acbc3daaa722781feb556990f4164033a3c
Regis <regis.lopes.eso@gmail.com>
2025-10-14 15:05:05 -0300

    Add Class 50: Reactivity with Ref

--- Vue_Course_Material/Project 6 - Task Manager - Initial/src/App.vue
+++ Vue_Course_Material/Project 6 - Task Manager - Initial/src/App.vue
@@ -1,44 +1,48 @@
 <script setup>
+  import { ref } from 'vue';
 
-const tasks= [
-    {
-      name: "Website design",
-      description: "Define the style guide, branding and create the webdesign on Figma.",
-      completed: true
-    },
-    {
-      name: "Website development",
-      description: "Develop the portfolio website using Vue JS.",
-      completed: false
-    },
-    {
-      name: "Hosting and infrastructure",
-      description: "Define hosting, domain and infrastructure for the portfolio website.",
-      completed: false
-    },
-    {
-      name: "Composition API",
-      description: "Learn how to use the composition API and how it compares to the options API.",
-      completed: true
-    },
-    {
-      name: "Pinia",
-      description: "Learn how to setup a store using Pinia.",
-      completed: true
-    },
-    {
-      name: "Groceries",
-      description: "Buy rice, apples and potatos.",
-      completed: false
-    },
-    {
-      name: "Bank account",
-      description: "Open a bank account for my freelance business.",
-      completed: false
-    }
-];
+  const appName = ref("My new task manager");
+
+  const tasks= [
+      {
+        name: "Website design",
+        description: "Define the style guide, branding and create the webdesign on Figma.",
+        completed: true
+      },
+      {
+        name: "Website development",
+        description: "Develop the portfolio website using Vue JS.",
+        completed: false
+      },
+      {
+        name: "Hosting and infrastructure",
+        description: "Define hosting, domain and infrastructure for the portfolio website.",
+        completed: false
+      },
+      {
+        name: "Composition API",
+        description: "Learn how to use the composition API and how it compares to the options API.",
+        completed: true
+      },
+      {
+        name: "Pinia",
+        description: "Learn how to setup a store using Pinia.",
+        completed: true
+      },
+      {
+        name: "Groceries",
+        description: "Buy rice, apples and potatos.",
+        completed: false
+      },
+      {
+        name: "Bank account",
+        description: "Open a bank account for my freelance business.",
+        completed: false
+      }
+  ];
 
 </script>
+
 
 <template>
 
@@ -46,9 +50,10 @@ const tasks= [
     <div class="header">
       <div class="header-side">
         <h1>
-          Tasks Manager
+          {{ appName }}
         </h1>
       </div>
+      <input type="text" v-model="appName">
     </div>
     
     <div class="filters">
@@ -115,6 +120,8 @@ const tasks= [
    
 
 </template>
+
+
 
 
 <style lang="scss" scoped>
